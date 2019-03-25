b6f14efd163d49e89a32ecc75861de07f46e09bf
chobich <cat.chobi0608@gmail.com>
2019-03-25 21:09:06 +0900

    deviseの修正

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,15 +1,9 @@
 class MembersController < ApplicationController
 
-  before_action :move_to_index, except: :index
-
   def index
   end
 
   def new
   end
 
-  private
-  def move_to_index
-    redirect_to action: :index unless member_signed_in?
-  end
 end
